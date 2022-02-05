4271ffb152df1917c0e4ea612b4427d0af49621e
ÖZKAN SEMİZ <ozknsmz@hotmail.com>
2022-02-05 18:58:29 +0300

    todo authentication jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,7 +13,12 @@ import { PrismaService } from 'src/prisma.service';
 import { User, Prisma } from '@prisma/client';
 import { UserDto } from './Dto/UserDto';
 
-
+/* // TODO: JWT Authentication: 
+  - https://progressivecoder.com/how-to-implement-nestjs-jwt-authentication-using-jwt-strategy/#:~:text=In%20this%20post%2C%20we%20will,as%20the%20token%20is%20valid.
+  - https://www.codemag.com/Article/2001081/Nest.js-Step-by-Step-Part-3-Users-and-Authentication
+  - https://www.passportjs.org/packages/
+  - https://www.youtube.com/watch?v=_L225zpUK0M&ab_channel=MariusEspejo
+*/
 @Controller('')
 export class UserController {
   constructor(
