c6b615021dbd2c814ebc5d5d262b58c2e0cbd8ce
ÖZKAN SEMİZ <ozknsmz@hotmail.com>
2022-02-13 19:18:17 +0300

    fixed guards for roles

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,23 +1,40 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Role } from 'src/Role/role.enum';
+import { Role_type, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 import { ROLES_KEY } from 'src/Role/roles.decorator';
+import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<Role_type[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
     if (!requiredRoles) {
       return true;
     }
+
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    // return true
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    const User = this.userService.findOneWithUsername(user.username);
+    // User.then((res) => {
+    //   // console.log(requiredRoles.some((role) => res.user_role?.includes(role)));
+    //   return requiredRoles.some((role) => res.user_role?.includes(role));
+    // });
+
+    return requiredRoles.some((role) =>
+      User.then((res) => {
+        requiredRoles.some((role) => res.user_role?.includes(role));
+      })
+    );
   }
 }
